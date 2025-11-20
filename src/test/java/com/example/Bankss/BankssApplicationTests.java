442088ca4d4ee6758f026d75b3afa14c02914a0f
BhanuPrasadYeligeti <123169497+BhanuPrasadYeligeti@users.noreply.github.com>
2025-11-20 23:01:52 +0530

    Update BankssApplicationTests.java

--- src/test/java/com/example/Bankss/BankssApplicationTests.java
+++ src/test/java/com/example/Bankss/BankssApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankssApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
