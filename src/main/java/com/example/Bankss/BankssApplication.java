901732c167ed167378e045506c8898f477f42697
BhanuPrasadYeligeti <bhanuprasadyeligeti@gmail.com>
2026-03-07 15:41:15 +0530

    fix: add SpringBootServletInitializer

--- src/main/java/com/example/Bankss/BankssApplication.java
+++ src/main/java/com/example/Bankss/BankssApplication.java
@@ -2,12 +2,18 @@ package com.example.Bankss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankssApplication {
+public class BankssApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BankssApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankssApplication.class, args);
 	}
-
 }
